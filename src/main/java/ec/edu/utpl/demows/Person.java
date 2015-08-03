ea26a5db7aa8eadcc2d919128e0c314c7e560156
Marlon <marsantovi@gmail.com>
2015-08-03 12:06:20 -0500

    Subiendo proyecto

---
+++ src/main/java/ec/edu/utpl/demows/Person.java
@@ -0,0 +1,37 @@
+package ec.edu.utpl.demows;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+
+@Entity
+@Table(name = "person")
+public class Person {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long id;
+	
+	@NotNull
+	private String firstName;
+	@NotNull
+	private String lastName;
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+}
