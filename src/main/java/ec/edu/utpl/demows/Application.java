4bdef6da90980ae04329ad181a040f318049c173
Marlon <marsantovi@gmail.com>
2015-08-04 12:53:50 -0500

    cambios para que el proyecto sea deplegable desde un tomcat no embebido

--- src/main/java/ec/edu/utpl/demows/Application.java
+++ src/main/java/ec/edu/utpl/demows/Application.java
@@ -2,11 +2,31 @@ package ec.edu.utpl.demows;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-public class Application {
+public class Application extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
+	
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class);
+    }
+	
+}
+
+@RestController
+class GreetingController {
+    
+    @RequestMapping("/hello/{name}")
+    String hello(@PathVariable String name) {
+        return "Hello, " + name + "!";
+    }
 }
